468110a639db31866ea783e3d1f1ce4531cc5307
Kaleigh Scruggs <kaleigh.scruggs@northhighland.com>
2019-07-19 14:05:30 -0400

    adding plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,60 @@ module.exports = {
     siteUrl: "https://kaleighcomedy.com",
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        useMozJpeg: false,
+        stripMetadata: true,
+        defaultQuality: 75,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-84017317-5",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `PT Sans`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/images/favicon.png",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `kaleigh comedy`,
+        short_name: `ks`,
+        start_url: `/`,
+        background_color: `#679499`,
+        theme_color: `#679499`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`,
+        image: `src/images/photo.jpg`,
+      },
+    },
     {
       resolve: `gatsby-theme-notes`,
       options: {
@@ -16,11 +70,5 @@ module.exports = {
     // with gatsby-plugin-theme-ui, the last theme in the config
     // will override the theme-ui context from other themes
     { resolve: `gatsby-theme-blog` },
-    {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: "./src/images/favicon.png",
-      },
-    },
   ],
 }
